8746662b77de23aa40bcfa7d7c1488fce587dd8b
Ganeshg2309 <ganeshgudlaman.sangamone@gmail.com>
2026-06-21 11:12:43 +0530

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,11 @@
+        #def calc1(num1,um1=n1+n2
+            dif1=n1-n2
+            prd1=n1*n2
+            div1=n1/n2
+            div2=n1//n2
+            rem1=n1%n2
+            exp1=n1**n2
+            print(sum1,dif1,prd1,div1,div2,rem1,exp1)
+        calc1(5,20)
+
+
---
+++ 2.py
@@ -0,0 +1,15 @@
+#def calc(num1,num2):
+    
+    sum1=num1+num2
+    dif1=num1-num2
+    prd1=num1*num2
+    div1=num1/num2
+    div2=num1//num2
+    ram1=num1%num2
+    exp1=num1**num2
+    print(sum1,dif1,prd1,div1,div2,ram1,exp1)
+calc(8,4)
+calc(8,5)
+calc(5,8)
+calc(10,2)
+calc(2,10)
